d5fc2c2c29806b458eccabafa166742937bd3731
Yue Zhang <architectyuezhang@gmail.com>
2022-04-14 14:53:45 -0700

    modified Row and CSS

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import axios from '../../server/axios';
+import requests from '../../server/Requests';
+import React, { useEffect, useState } from 'react';
 import "./Banner.css";
 
 function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+
+        fetchData();
+    }, [])
+
+    console.log(movie);
+
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n - 1) + '...' : string;
     }
@@ -9,26 +29,19 @@ function Banner() {
     return (
     <header className='banner' style={{
         backgroundSize: "cover",
-        backgroundImage:`url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+        backgroundImage:`url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
         backgroundPosition: "center center",
     }}>
         <div className="banner_contents">
             <h1 className="banner_title">
-                Movie Name
+                {movie?.title || movie?.name || movie?.original_name}
             </h1>
             <div className='banner_buttons'>
                 <button className="banner_button">Play</button>
                 <button className="banner_button">My List</button>
             </div>
             <h1 className="banner_description">
-                {truncate(`This is a description for testing purpose This is a description for testing purpose This is a description for testing purpose
-                This is a description for testing purpose This is a description for testing purpose 
-                This is a description for testing purpose This is a description for testing purpose
-                This is a description for testing purpose This is a description for testing purpose
-                This is a description for testing purpose This is a description for testing purpose
-                This is a description for testing purpose This is a description for testing purpose
-                This is a description for testing purpose This is a description for testing purpose
-                `, 100)}
+                {truncate(movie?.overview, 100)}
             </h1>
         </div>
 
