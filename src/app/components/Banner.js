4fd8b52a2192230d9b2daa7adc9f2f10c913c2b8
Yue Zhang <architectyuezhang@gmail.com>
2022-04-14 11:16:59 -0700

    first commit

---
+++ src/app/components/Banner.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className='banner' style={{
+        backgroundSize: "cover",
+        backgroundImage:`url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+        backgroundPosition: "center center",
+    }}>
+        <div className="banner_contents">
+            <h1 className="banner_title">
+                Movie Name
+            </h1>
+            <div className='banner_buttons'>
+                <button className="banner_button">Play</button>
+                <button className="banner_button">My List</button>
+            </div>
+            <h1 className="banner_description">
+                This is a description for testing purpose
+            </h1>
+        </div>
+
+        <div className="banner--fadeBottom" />
+    </header>
+  )
+}
+
+export default Banner;
